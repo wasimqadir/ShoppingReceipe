d9677964d0d136ad4eae5501fbde5ff77eb2f674
Wasim Qadir <wasim.qadir@maqta.ae>
2023-05-03 22:01:40 +0400

    initial commit for shopping receipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ReceipesComponent } from './receipes/receipes.component';
+import { RecipeDetailComponent } from './receipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './receipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './receipes/recipe-list/recipe-item/recipe-item.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReceipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
