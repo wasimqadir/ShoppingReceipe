72f9e2035e13a2c666fceb53c1cd0b489f1448d3
Wasim Qadir <wasim.qadir@maqta.ae>
2023-05-04 13:26:04 +0400

    added recipe and shopping list components

--- src/app/receipes/receipes.component.ts
+++ src/app/receipes/receipes.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-receipes',
-  templateUrl: './receipes.component.html',
-  styleUrls: ['./receipes.component.css']
+	selector: 'app-receipes',
+	templateUrl: './receipes.component.html',
+	styleUrls: ['./receipes.component.css']
 })
 export class ReceipesComponent {
- 
+
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../shared/Ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
-
+  ingredients: Ingredient[] = [
+    new Ingredient("Apples",4),
+    new Ingredient("Tomatoes",3)
+  ];
 }
