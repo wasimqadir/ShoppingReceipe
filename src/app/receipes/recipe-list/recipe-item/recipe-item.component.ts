5a6a7e9d12f233e4bffa5b10abea54896fb288df
Wasim Qadir <wasim.qadir@maqta.ae>
2023-05-05 11:40:11 +0400

    replaced existing implementation of recipe with a service

--- src/app/receipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/receipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
+import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -8,9 +9,14 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent {
   @Input() RecipeItem: Recipe;
-  @Output() selectedRecipeEmitter = new EventEmitter<Recipe>();
+  //@Output() selectedRecipeEmitter = new EventEmitter<Recipe>();
+
+  constructor(private recipeService: RecipeService) {
+		
+	}
 
   onSelectedRecipe() {
-    this.selectedRecipeEmitter.emit(this.RecipeItem);
+    this.recipeService.SelectedRecipeChanged.emit(this.RecipeItem);
+    //this.selectedRecipeEmitter.emit(this.RecipeItem);
   }
 }
